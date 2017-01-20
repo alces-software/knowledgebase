50985c6a198b864605863b84b34f1603fafef436
Steve Norledge <steve.norledge@alces-software.com>
2017-01-20 15:08:17 +0000

    - bootstrap wip

--- epel/scripts/bootstrap/bootstrap.sh
+++ epel/scripts/bootstrap/bootstrap.sh
@@ -11,5 +11,14 @@ yum -y install git
 
 mkdir -p /var/www/html/
 cd /var/www/html
+if [ -d ${_ALCES_CLUSTER} ]; then
+  mv -v ${_ALCES_CLUSTER} ${_ALCES_CLUSTER}-`date +%F_%T`
+fi
 git clone https://github.com/alces-software/knowledgebase.git ${_ALCES_CLUSTER}
+ln -snf ${_ALCES_CLUSTER}/epel epel
 
+
+KBPATH=/var/www/html/${_ALCES_CLUSTER}/epel/
+
+cp -v /root/.alcesconf ${KBPATH}/conf/${_ALCES_CLUSTER}
+ln -snf ${KBPATH}/conf/${_ALCES_CLUSTER} ${KBPATH}/conf/config
