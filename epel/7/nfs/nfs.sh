f1b98ef42bd7cad58e604c69e1aaa194e1f25d59
Stu Franks <stu.j.franks@gmail.com>
2017-07-13 14:36:15 +0100

    Modified to 'is_server' check

--- epel/7/nfs/nfs.sh
+++ epel/7/nfs/nfs.sh
@@ -1,34 +1,36 @@
 yum -y install nfs-utils
 
-<% if networks.pri.ip == netconfig.nfsserver -%>
+<% if nfsconfig.is_server -%>
 # Create export directories
-<% nfsmounts.each do | mount, path | -%>
+<% nfsexports.each do | path, opts | -%>
 mkdir -p <%= path %>
 <% end -%>
 
 # Increase nfsd thread count
 sed -ie "s/^#\RPCNFSDCOUNT.*$/\RPCNFSDCOUNT=32/g" /etc/sysconfig/nfs
 
+EXPORTOPTS="<%= networks.pri.network %>/<%= networks.pri.netmask %>(rw,no_root_squash,sync)"
+
 EXPORTS=`cat << EOF
-<% nfsmounts.each do | mount, path | -%>
-<%= path %>    <%= networks.pri.network %>/<%= networks.pri.netmask %>(rw,no_root_squash,sync)
+<% nfsexports.each do | path, opts | -%>
+<%= path %>   <%= if defined?(opts.options) then opts.options else "#{networks.pri.network}/#{networks.pri.netmask}(rw,no_root_squash,sync)" end %>
 <% end -%>
 EOF`
 
-echo "$EXPORTS" >> /etc/exports
+echo "$EXPORTS" > /etc/exports
 
 <% else -%>
 
 MOUNTS=`cat << EOF
 <% nfsmounts.each do | mount, path | -%>
-<%= netconfig.nfsserver %>:<%= path %>    <%= path %>    nfs    intr,rsize=32768,wsize=32768,_netdev    0 0
+<%= path.server %>:<%= path.export %>    <%= mount %>    nfs    <%= if defined?(path.options) then path.options else 'intr,rsize=32768,wsize=32768,_netdev' end -%>    0 0
 <% end -%>
 EOF`
 
 echo "$MOUNTS" >> /etc/fstab
 
 <% nfsmounts.each do | mount, path | -%>
-mkdir -p <%= path %>
+mkdir -p <%= path.target %>
 <% end -%>
 
 <% end -%>
