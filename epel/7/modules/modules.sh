8c8c9746ac3c3f1ff7f8d9865b1aa7c7e2f17ce6
Stu Franks <stu.j.franks@gmail.com>
2017-08-04 16:24:29 +0100

    First draft of modules script

---
+++ epel/7/modules/modules.sh
@@ -0,0 +1,10 @@
+yum install environment-modules
+
+<% if modules.is_server -%>
+mkdir <%= modules.directory %>
+echo '<%= modules.directory %>    <%= networks.pri.network %>/<%= networks.pri.netmask %>(rw,no_root_squash,sync)'
+exportfs -a
+<% end -%>
+
+echo '<%= modules.directory %>' >> /usr/share/Modules/init/.modulespath
+echo '<%= modules.server %>:<%= modules.directory %>  <%= modules.directory %>  nfs  defaults  0 0' >> /etc/fstab
