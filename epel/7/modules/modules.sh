58bc9ebb13ba954520525ddcdcc908824a666863
Stu Franks <stu.j.franks@gmail.com>
2017-08-08 09:52:36 +0100

    Added subdirectory to apps directory

--- epel/7/modules/modules.sh
+++ epel/7/modules/modules.sh
@@ -1,12 +1,14 @@
 yum install -y environment-modules
 
+MODULESDIR="<%= modules.directory %>/modulefiles"
+
 <% if modules.is_server -%>
-mkdir <%= modules.directory %>
+mkdir $MODULESDIR
 echo '<%= modules.directory %>    <%= networks.pri.network %>/<%= networks.pri.netmask %>(rw,no_root_squash,sync)'
 exportfs -a
 <% end -%>
 
-echo '<%= modules.directory %>' >> /usr/share/Modules/init/.modulespath
+echo "$MODULESDIR" >> /usr/share/Modules/init/.modulespath
 <% if ! modules.is_server -%>
 echo '<%= modules.server %>:<%= modules.directory %>  <%= modules.directory %>  nfs  defaults  0 0' >> /etc/fstab
 <% end -%>
