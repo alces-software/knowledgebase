5f748940b8765c1fefb98ad4e182b6e33440e345
Stu Franks <stu.j.franks@gmail.com>
2017-08-14 12:27:31 +0100

    Syslog script

---
+++ epel/7/syslog/syslog.sh
@@ -0,0 +1,31 @@
+yum install -y rsyslog
+
+<% if networks.pri.ip == alces.hostip -%>
+cat << EOF > /etc/rsyslog.d/metalware.conf
+\$template remoteMessage, "/var/log/slave/%FROMHOST%/messages.log"
+:fromhost-ip, !isequal, "127.0.0.1" ?remoteMessage
+& ~
+EOF
+
+sed -i -e "s/^#\$ModLoad imudp.*$/\$ModLoad imudp/g" /etc/rsyslog.conf
+sed -i -e "s/^#\$UDPServerRun 514.*$/\$UDPServerRun 514/g" /etc/rsyslog.conf
+sed -i -e "s/^#\$ModLoad imtcp.*$/\$ModLoad imtcp/g" /etc/rsyslog.conf
+sed -i -e "s/^#\$InputTCPServerRun 514.*$/\$InputTCPServerRun 514/g" /etc/rsyslog.conf
+
+cat << EOF > /etc/logrotate.d/rsyslog-remote
+/var/log/slave/*/*.log {
+    sharedscripts
+    compress
+    rotate 2
+    postrotate
+        /bin/kill -HUP \`cat /var/run/syslogd.pid 2> /dev/null\` 2> /dev/null || true
+        /bin/kill -HUP \`cat /var/run/rsyslogd.pid 2> /dev/null\` 2> /dev/null || true
+    endscript
+}
+EOF
+<% else -%>
+echo '*.* @<%= alces.hostip %>:514' >> /etc/rsyslog.conf
+<% end -%>
+
+systemctl enable rsyslog
+systemctl restart rsyslog
