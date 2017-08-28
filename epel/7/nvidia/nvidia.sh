5837fe98fc1942e323327d5677ad0152617b22d6
Steve Norledge <steve.norledge@alces-software.com>
2017-08-28 18:55:23 +0100

    /etc/sysconfig/grub is a link to /etc/default/grub, this sed breaks the link

--- epel/7/nvidia/nvidia.sh
+++ epel/7/nvidia/nvidia.sh
@@ -1,5 +1,5 @@
 <% if nvidia -%>
-sed -i "s/GRUB_CMDLINE_LINUX=\"\(.*\)\"/GRUB_CMDLINE_LINUX=\"\1 rdblacklist=nouveau blacklist=nouveau\"/" /etc/sysconfig/grub
+sed -i "s/GRUB_CMDLINE_LINUX=\"\(.*\)\"/GRUB_CMDLINE_LINUX=\"\1 rdblacklist=nouveau blacklist=nouveau\"/" /etc/default/grub
 grub2-mkconfig > /etc/grub2.cfg
 mkinitrd --force /boot/initramfs-`uname -r`.img `uname -r`
 echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
@@ -8,6 +8,7 @@ rmmod -v nouveau
 
 yum -y groupinstall "Development Tools"
 
+mkdir -p /var/lib/firstrun/scripts/
 cat << EOF > /var/lib/firstrun/scripts/nvidia.bash
 URL=http://<%= alces.hostip %>/installers/
 curl \$URL/nvidia.run > /tmp/nvidia.run
