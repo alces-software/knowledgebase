1ae5b740d53a6c07c899bc6111131d823788022d
Stu Franks <stu.j.franks@gmail.com>
2017-09-27 17:42:48 +0100

    Rework repos.sh around repoman setup

--- epel/7/repo/repos.sh
+++ epel/7/repo/repos.sh
@@ -1,118 +1,37 @@
-<% repotype = eval(localmirror.to_s) %>
-REPOS=`cat << EOF
-<% repotype.each do | name, repo | -%>
-<% next if name.to_s == 'repourl' -%>
-[<%= name %>]
-name=<%= name %>
-baseurl=<%= repo.baseurl %>
-description=<%= if defined?(repo.description) then repo.description else 'No description specified' end %>
-enabled=<%= if defined?(repo.enabled) then repo.enabled else 1 end %>
-skip_if_unavailable=<%= if defined?(repo.skip_if_unavailable) then repo.skip_if_unavailable else 1 end %>
-gpgcheck=<%= if defined?(repo.gpgcheck) then repo.gpgcheck else 0 end %>
-priority=<%= if defined?(repo.priority) then repo.priority else 10 end %>
+<% if repo.is_server -%>
+# Download repoman
+cd /opt/
+git clone https://github.com/alces-software/repoman.git
 
-<% end -%>
-[<%= customrepo.custom.name %>]
-name=<%= customrepo.custom.name %>
-baseurl=<%= customrepo.custom.baseurl %>
-description=<%= if defined?(customrepo.custom.description) then customrepo.custom.description else 'No description specified' end %>
-enabled=<%= if defined?(customrepo.custom.enabled) then customrepo.custom.enabled else 1 end %>
-skip_if_unavailable=<%= if defined?(customrepo.custom.skip_if_unavailable) then customrepo.custom.skip_if_unavailable else 1 end %>
-gpgcheck=<%= if defined?(customrepo.custom.gpgcheck) then customrepo.custom.gpgcheck else 0 end %>
-priority=<%= if defined?(customrepo.custom.priority) then customrepo.custom.priority else 10 end %>
-EOF`
+# Add additional repositories
+mkdir -p /var/lib/repoman/templates/centos/7/
+cd /var/lib/repoman/templates/centos/7/
+REPOFILES="base.alcesaws base.alcesinternal lustre.alcesaws lustre.alcesinternal"
+for repofile in $REPOFILES ; do
+  wget https://raw.githubusercontent.com/alces-software/knowledgebase/master/epel/7/repo/$repofile
+done
 
-REPOPATH=<%= repoconfig.repopath %>
+# Mirror repos
+/opt/repoman/repoman.rb mirror --distro centos7 --include <%= mirrorrepos %> --reporoot /opt/alces/repo --configurl http://<%= alces.nodename %>/repo/ --configout /opt/alces/repo/client.repo
 
-yum install -y yum-plugin-priorities yum-utils
-
-<% if repoconfig.is_server && localmirror == 'localrepos' then -%>
-# Install necessary packages and enable service
-yum -y install createrepo httpd
-systemctl enable httpd.service
-
-# Setup directories
-if [ ! -d /opt/alces ]; then 
-    mkdir -p /opt/alces
-fi
-
-cd /opt/alces
-
-if [ ! -d $REPOPATH ] ; then
-    mkdir -p $REPOPATH
-fi
-
-if [ ! -d installers ] ; then
-    mkdir -p installers
-fi
-
-cd $REPOPATH
-
-# Yum config settings
-cat << EOF > yum.conf
-[main]
-cachedir=/var/cache/yum/\$basearch/\$releasever
-keepcache=0
-debuglevel=2
-logfile=/var/log/yum.log
-exactarch=1
-obsoletes=1
-gpgcheck=1
-plugins=1
-installonly_limit=5
-bugtracker_url=http://bugs.centos.org/set_project.php?project_id=23&ref=http://bugs.centos.org/bug_report_page.php?category=yum
-distroverpkg=centos-release
-reposdir=/dev/null
-EOF
-
-# Add upstream repos to yum.conf
-cat << EOF >> yum.conf
-<% mirrorrepos = eval(repoconfig.mirrorfrom.to_s) %>
-<% mirrorrepos.each do | name, repo | -%>
-<% next if name.to_s == 'repourl' -%>
-[<%= name %>]
-name=<%= name %>
-baseurl=<%= repo.baseurl %>
-description=<%= if defined?(repo.description) then repo.description else 'No description specified' end %>
-enabled=<%= if defined?(repo.enabled) then repo.enabled else 1 end %>
-skip_if_unavailable=<%= if defined?(repo.skip_if_unavailable) then repo.skip_if_unavailable else 1 end %>
-gpgcheck=<%= if defined?(repo.gpgcheck) then repo.gpgcheck else 0 end %>
-priority=<%= if defined?(repo.priority) then repo.priority else 10 end %>
-
-<% end -%>
-EOF
-
-# Setup HTTPD server config file
+# HTTP setup
 cat << EOF > /etc/httpd/conf.d/repo.conf
-<Directory /opt/alces/$REPOPATH/>
+<Directory /opt/alces/repo/>
     Options Indexes MultiViews FollowSymlinks
     AllowOverride None
     Require all granted
     Order Allow,Deny
     Allow from <%= networks.pri.network %>/255.255.0.0
 </Directory>
-Alias /repo /opt/alces/$REPOPATH
+Alias /repo /opt/alces/repo
 EOF
-
 systemctl restart httpd.service
 
-# Build repository
-reposync -nm --config yum.conf -r centos
-mkdir -p centos/LiveOS
-## NOTE: Could this URL be ERBified?? ##
-curl <%= eval(repoconfig.mirrorfrom.to_s).centos.baseurl %>/LiveOS/squashfs.img > centos/LiveOS/squashfs.img
-
-reposync -nm --config yum.conf -r centos-updates
-reposync -nm --config yum.conf -r centos-extras
-reposync -nm --config yum.conf -r epel
-
-createrepo -g comps.xml centos
-createrepo centos-updates
-createrepo centos-extras
-createrepo -g comps.xml epel
-
 <% else -%>
+<%     if repoconfig.clientrepofile -%>
 find /etc/yum.repos.d/*.repo -exec mv -fv {} {}.bak \;
-echo "$REPOS" > /etc/yum.repos.d/cluster.repo
+curl <%= repoconfig.clientrepofile %> > /etc/yum.repos.d/cluster.repo
 yum clean all
+<%     end -%>
 <% end -%>
+
